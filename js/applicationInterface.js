a821bce20ff8a773c941e2c4888652ef0bbcac47
Александр <leks511@bk.ru>
2019-10-11 21:51:22 +0300

    Добавь обработку запроса на ajax

--- js/applicationInterface.js
+++ js/applicationInterface.js
@@ -2,34 +2,18 @@
 
 (function () {
   // Данные для включения интерфейса
+  var main = document.querySelector('main');
   var applicationMap = document.querySelector('.map');
   var formAddingYourAdvertise = document.querySelector('.ad-form');
   var fieldsetsFromFormAddingYourAdvertise = formAddingYourAdvertise.querySelectorAll('fieldset');
   var mainMapPin = document.querySelector('.map__pin--main');
   var formWithMapFilters = document.querySelector('.map__filters');
   var mapFiltersFormSelectsAndFieldset = formWithMapFilters.querySelectorAll('fieldset, select');
-
   // Данные для добавления пинов после включения интерфейса
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var mapPinsList = document.querySelector('.map__pins');
+  var onErrorTemplate = document.querySelector('#error').content.querySelector('.error');
 
-  var typesList = ['palace', 'flat', 'house', 'bungalo'];
-  var checkinsList = ['12:00', '13:00', '14:00'];
-  var checkoutsList = ['12:00', '13:00', '14:00'];
-  var photosList = [
-    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-  ];
-  var featuresList = [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-    'conditioner'
-  ];
-  var advertisementsList = createAdvertisementsList();
 
   // Функции для включения/выключения интерфейса
 
@@ -57,22 +41,14 @@
     applicationMap.classList.remove('map--faded');
     formAddingYourAdvertise.classList.remove('ad-form--disabled');
 
-    // При включении интерфейса запускаем функцию рендеринга пинов на карте
-    createPins(advertisementsList);
 
+    // При включении интерфейса получаем данные с сервера и запускаем рендеринг пинов при успехе
+    window.backend.load(createPins, onLoadError);
+
+    // Активируем формы сортировки
     removeDisabledAttributeFromFormElements(fieldsetsFromFormAddingYourAdvertise);
     removeDisabledAttributeFromFormElements(mapFiltersFormSelectsAndFieldset);
   }
-
-  mainMapPin.addEventListener('mousedown', function () {
-    activateApplicationInterface();
-  });
-
-  mainMapPin.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === window.util.ENTER_KEYCODE) {
-      activateApplicationInterface();
-    }
-  });
 
   // Функции для отрисовки пинов на странице
   function createPinElement(pin) {
@@ -95,35 +71,23 @@
     mapPinsList.appendChild(fragmentForPins);
   }
 
-  function createAdvertisementsList() {
-    var list = [];
-
-    for (var i = 0; i < 8; i++) {
-      list[i] = {
-        'author': {
-          'avatar': 'img/avatars/user0' + (i + 1) + '.png'
-        },
-        'offer': {
-          'title': 'Заголовок ' + (i + 1),
-          'address': '600, 350',
-          'price': window.util.getRandomNumber(100, 500),
-          'type': window.util.createRandomArray(typesList),
-          'rooms': window.util.getRandomNumber(1, 7),
-          'guests': window.util.getRandomNumber(1, 12),
-          'checkin': window.util.createRandomArray(checkinsList),
-          'checkout': window.util.createRandomArray(checkoutsList),
-          'features': window.util.createRandomArray(featuresList),
-          'description': 'Здесь будет описание',
-          'photos': window.util.createRandomArray(photosList)
-        },
-        'location': {
-          'x': window.util.getRandomNumber(0, 1200),
-          'y': window.util.getRandomNumber(130, 630)
-        }
-      };
-    }
-    return list;
+  function onLoadError(errorMessage) {
+    var elementWithErrorText = onErrorTemplate.cloneNode(true);
+    elementWithErrorText.querySelector('.error__message').textContent = errorMessage;
+    main.appendChild(elementWithErrorText);
   }
+
+  // Прослушка активации SPA при нажатии на главный пин
+
+  mainMapPin.addEventListener('mousedown', function () {
+    activateApplicationInterface();
+  });
+
+  mainMapPin.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.util.ENTER_KEYCODE) {
+      activateApplicationInterface();
+    }
+  });
 
   // Изначально интерфейс отключен. Запустим сразу функцию, отключающую интерфейс
   inactivateApplicationInterface();
