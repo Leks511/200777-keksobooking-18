8be870d58e43a476734d7a516d53a6b0fc56ec9d
Александр <leks511@bk.ru>
2019-10-28 20:59:59 +0300

    Задание почти выполнено: осталось дописать валидацию количества комнат и мест

--- js/validation.js
+++ js/validation.js
@@ -1,34 +1,93 @@
 'use strict';
 
+// ОСТАЛОСЬ ДОПИСАТЬ ВАЛИДАЦИЮ КОЛИЧЕСТВА КОМНАТ И КОЛИЧЕСТВА МЕСТ ПРИ ОТПРАВКЕ!!!!
+
 (function () {
+  var minPrices = {
+    BUNGALO: 0,
+    FLAT: 1000,
+    HOUSE: 5000,
+    PALACE: 10000
+  };
+
   var adFormElement = document.querySelector('.ad-form');
-  var roomsElement = document.querySelector('#room_number');
-  var guestsElement = document.querySelector('#capacity');
+  var roomsElement = adFormElement.querySelector('#room_number');
+  var guests = adFormElement.querySelectorAll('#capacity option');
+
+  var typeElement = adFormElement.querySelector('#type');
+  var priceElement = adFormElement.querySelector('#price');
+
+  var timeinElement = adFormElement.querySelector('#timein');
+  var timeoutElement = adFormElement.querySelector('#timeout');
+
+  // Функция, добавляющая disabled недоступному полю
+  function setDisable(element) {
+    element.setAttribute('disabled', 'disabled');
+  }
 
   // Функция обработки изменений полей гостей и комнат
-  function handleRoomsGeustsChecking(evt) {
-    var roomsValue = roomsElement.value;
-    var guestsValue = guestsElement.value;
+  function handleRoomsGeustsChecking(rooms) {
+    // Уберём disabled у всех элементов для того, чтобы после добавить в соответствии с изменённым значением
+    guests.forEach(function (option) {
+      option.removeAttribute('disabled');
+    });
 
-    if (roomsValue !== guestsValue) {
-      evt.preventDefault();
-
-      roomsElement.setCustomValidity('Количество гостей должно соответствовать количеству комнат');
+    if (rooms === 100) {
+      for (var i = 0; i < guests.length; i++) {
+        if (parseInt(guests[i].value, 10) > 0) {
+          setDisable(guests[i]);
+        }
+      }
     } else {
-      roomsElement.setCustomValidity('');
+      setDisable(guests[guests.length - 1]);
+
+      for (var j = 0; j < guests.length; j++) {
+        if (parseInt(guests[j].value, 10) > rooms) {
+          setDisable(guests[j]);
+        }
+      }
     }
   }
 
   // Обработка гостей и комнат по изменению значения
   roomsElement.addEventListener('change', function () {
     roomsElement.setCustomValidity('');
+
+    var roomsQuantity = parseInt(roomsElement.value, 10);
+
+    handleRoomsGeustsChecking(roomsQuantity);
   });
 
-  // Обработка гостей и комнат по изменению значения
-  guestsElement.addEventListener('change', function () {
-    roomsElement.setCustomValidity('');
+  // При изменении поля "Тип" задаётся соответствующее минимальное значение для поля "Цена за ночь"
+  typeElement.addEventListener('change', function () {
+    if (typeElement.value === 'bungalo') {
+      priceElement.setAttribute('min', minPrices.BUNGALO);
+    }
+
+    if (typeElement.value === 'flat') {
+      priceElement.setAttribute('min', minPrices.FLAT);
+    }
+
+    if (typeElement.value === 'house') {
+      priceElement.setAttribute('min', minPrices.HOUSE);
+    }
+
+    if (typeElement.value === 'palace') {
+      priceElement.setAttribute('min', minPrices.PALACE);
+    }
   });
 
-  // Обработка гостей и комнат по отправке формы
-  adFormElement.addEventListener('submit', handleRoomsGeustsChecking);
+  // Функция, задающая аналогичное значение элементу въезда/выезда
+  function equalizeTimeValue(element1, element2) {
+    element2.value = element1.value;
+  }
+
+  // Синхронизуем поля даты въезда/выезда
+  timeinElement.addEventListener('change', function () {
+    equalizeTimeValue(timeinElement, timeoutElement);
+  });
+  timeoutElement.addEventListener('change', function () {
+    equalizeTimeValue(timeoutElement, timeinElement);
+  });
+
 })();
